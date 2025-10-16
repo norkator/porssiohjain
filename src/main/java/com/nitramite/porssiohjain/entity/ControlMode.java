737e68f6f41eaf432a93d1d2bce792c43636f2db
Martin Kankaanranta <martin.kankaanranta@hotmail.com>
2025-10-16 19:04:34 +0300

    change one of modes

--- src/main/java/com/nitramite/porssiohjain/entity/ControlMode.java
+++ src/main/java/com/nitramite/porssiohjain/entity/ControlMode.java
@@ -2,7 +2,7 @@ package com.nitramite.porssiohjain.entity;
 
 public enum ControlMode {
     BELOW_MAX_PRICE, // turns ON when price < maxPriceSnt
-    ABOVE_MAX_PRICE, // turns OFF when price > maxPriceSnt (inverse)
+    CHEAPEST_HOURS,  // daily cheapest hours, control on based on dailyOnMinutes
     MANUAL,          // manual override, use manualOn field
     SCHEDULED        // user defined schedule
 }
