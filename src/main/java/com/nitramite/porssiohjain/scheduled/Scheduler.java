296e56723df581e5e97a9f72743d3d41f31caa82
Martin Kankaanranta <martin.kankaanranta@hotmail.com>
2025-10-22 14:37:19 +0300

    add secondary nordpool fetch and plan generation

--- src/main/java/com/nitramite/porssiohjain/scheduled/Scheduler.java
+++ src/main/java/com/nitramite/porssiohjain/scheduled/Scheduler.java
@@ -38,4 +38,19 @@ public class Scheduler {
         controlSchedulerService.generatePlannedForTomorrow();
     }
 
+    @Scheduled(cron = "0 0 18 * * *", zone = "Europe/Helsinki")
+    public void fetchNordpoolDataDailyBackup() {
+        try {
+            NordpoolResponse response = nordpoolDataPortalService.fetchData();
+            log.info("Nordpool day ahead data fetched and saved successfully: {}", response);
+        } catch (Exception e) {
+            log.error("Error fetching Nordpool data", e);
+        }
+    }
+
+    @Scheduled(cron = "0 40 18 * * *")
+    public void runAfterNordpoolImportBackup() {
+        controlSchedulerService.generatePlannedForTomorrow();
+    }
+
 }
