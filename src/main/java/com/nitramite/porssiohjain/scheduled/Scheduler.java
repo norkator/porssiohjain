3bb2b530491d0de8ddf16fad1ba259c7f1637f66
norkator <martin.kankaanranta@hotmail.com>
2025-11-24 20:02:03 +0200

    add scheduling for fingrid data loading

--- src/main/java/com/nitramite/porssiohjain/scheduled/Scheduler.java
+++ src/main/java/com/nitramite/porssiohjain/scheduled/Scheduler.java
@@ -5,6 +5,7 @@ import com.nitramite.porssiohjain.services.FingridDataService;
 import com.nitramite.porssiohjain.services.NordpoolDataPortalService;
 import com.nitramite.porssiohjain.services.SystemLogService;
 import com.nitramite.porssiohjain.services.models.NordpoolResponse;
+import com.nitramite.porssiohjain.services.models.WindForecastResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -15,28 +16,38 @@ public class Scheduler {
 
     private final NordpoolDataPortalService nordpoolDataPortalService;
     private final ControlSchedulerService controlSchedulerService;
-    private final SystemLogService systemLogService;
+    // private final SystemLogService systemLogService;
     private final FingridDataService fingridDataService;
 
     public Scheduler(
             NordpoolDataPortalService nordpoolDataPortalService,
             ControlSchedulerService controlSchedulerService,
-            SystemLogService systemLogService,
+            // SystemLogService systemLogService,
             FingridDataService fingridDataService
     ) {
         this.nordpoolDataPortalService = nordpoolDataPortalService;
         this.controlSchedulerService = controlSchedulerService;
-        this.systemLogService = systemLogService;
+        // this.systemLogService = systemLogService;
         this.fingridDataService = fingridDataService;
         // nordpoolDataPortalService.fetchData();
-        fingridDataService.fetchData();
+        // fingridDataService.fetchData();
+    }
+
+    @Scheduled(cron = "0 0 14 * * *", zone = "Europe/Helsinki")
+    public void fetchFingridDataDaily() {
+        try {
+            WindForecastResponse response = fingridDataService.fetchData();
+            log.info("Fingrid wind forecast data fetched and saved {} rows successfully", response.getData().size());
+        } catch (Exception e) {
+            log.error("Error fetching Fingrid wind forecast data", e);
+        }
     }
 
     @Scheduled(cron = "0 30 14 * * *", zone = "Europe/Helsinki")
     public void fetchNordpoolDataDaily() {
         try {
             NordpoolResponse response = nordpoolDataPortalService.fetchData();
-            log.info("Nordpool day ahead data fetched and saved successfully: {}", response);
+            log.info("Nordpool day ahead data fetched and saved {} rows successfully", response.getMultiIndexEntries().size());
         } catch (Exception e) {
             log.error("Error fetching Nordpool data", e);
         }
