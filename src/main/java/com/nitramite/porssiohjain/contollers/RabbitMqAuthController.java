6e8da0e53965ef62da69607dd14dd96a66ce3fee
norkator <martin.kankaanranta@hotmail.com>
2026-03-16 19:07:24 +0200

    add rabbitmq controller logging

--- src/main/java/com/nitramite/porssiohjain/contollers/RabbitMqAuthController.java
+++ src/main/java/com/nitramite/porssiohjain/contollers/RabbitMqAuthController.java
@@ -5,6 +5,7 @@ import com.nitramite.porssiohjain.entity.repository.DeviceRepository;
 import com.nitramite.porssiohjain.services.models.RabbitMqAuthResponse;
 import com.nitramite.porssiohjain.services.models.RabbitMqUserAuthRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/rabbitmq/authenticate")
 @RequiredArgsConstructor
+@Slf4j
 public class RabbitMqAuthController {
 
     private final DeviceRepository deviceRepository;
@@ -27,12 +29,15 @@ public class RabbitMqAuthController {
     ) {
         Optional<DeviceEntity> deviceOpt = deviceRepository.findByMqttUsername(request.getUsername());
         if (deviceOpt.isEmpty()) {
+            log.warn("MQTT auth denied for username '{}' - device not found", request.getUsername());
             return ResponseEntity.ok(new RabbitMqAuthResponse("deny"));
         }
         DeviceEntity device = deviceOpt.get();
         if (!Objects.equals(device.getMqttPassword(), request.getPassword())) {
+            log.warn("MQTT auth denied for username '{}' - invalid password", request.getUsername());
             return ResponseEntity.ok(new RabbitMqAuthResponse("deny"));
         }
+        log.info("MQTT auth allowed for username '{}' (device uuid: {})", request.getUsername(), device.getUuid());
         return ResponseEntity.ok(new RabbitMqAuthResponse("allow"));
     }
 
