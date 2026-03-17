2a73d86122e0dbe4257207a77208e9cdb204b85e
Martin Kankaanranta <martin.kankaanranta@hotmail.com>
2026-03-17 11:46:11 +0200

    modification

--- src/main/java/com/nitramite/porssiohjain/contollers/RabbitMqAuthController.java
+++ src/main/java/com/nitramite/porssiohjain/contollers/RabbitMqAuthController.java
@@ -32,15 +32,15 @@ public class RabbitMqAuthController {
         Optional<DeviceEntity> deviceOpt = deviceRepository.findByMqttUsername(username);
         if (deviceOpt.isEmpty()) {
             log.warn("MQTT auth denied for username '{}' - device not found", username);
-            return ResponseEntity.ok(new RabbitMqAuthResponse("deny"));
+            return ResponseEntity.ok(new RabbitMqAuthResponse(false));
         }
         DeviceEntity device = deviceOpt.get();
         if (!Objects.equals(device.getMqttPassword(), password)) {
             log.warn("MQTT auth denied for username '{}' - invalid password", username);
-            return ResponseEntity.ok(new RabbitMqAuthResponse("deny"));
+            return ResponseEntity.ok(new RabbitMqAuthResponse(false));
         }
         log.info("MQTT auth allowed for username '{}' (device uuid: {})", username, device.getUuid());
-        return ResponseEntity.ok(new RabbitMqAuthResponse("allow"));
+        return ResponseEntity.ok(new RabbitMqAuthResponse(true));
     }
 
 }
