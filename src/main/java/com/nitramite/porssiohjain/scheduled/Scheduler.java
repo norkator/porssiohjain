7d14997b32b19f89580bf5c3bcdb2feb7d3c0271
norkator <martin.kankaanranta@hotmail.com>
2026-04-07 19:43:06 +0300

    license change

--- src/main/java/com/nitramite/porssiohjain/scheduled/Scheduler.java
+++ src/main/java/com/nitramite/porssiohjain/scheduled/Scheduler.java
@@ -2,16 +2,11 @@
  * Pörssiohjain - Energy usage optimization platform
  * Copyright (C) 2026  Martin Kankaanranta / Nitramite Tmi
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, version 3.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * This source code is licensed under the Pörssiohjain Personal Use License v1.0.
+ * Private self-hosting for personal household use is permitted.
+ * Commercial use, resale, managed hosting, or offering the software as a
+ * service to third parties requires separate written permission.
+ * See LICENSE for details.
  */
 
 package com.nitramite.porssiohjain.scheduled;
