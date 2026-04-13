980c80146035cdd546549d828acf0cdc2827fbdc
Martin Kankaanranta <martin.kankaanranta@hotmail.com>
2026-04-13 22:07:41 +0300

    Update WeatherControlService.java

--- src/main/java/com/nitramite/porssiohjain/services/WeatherControlService.java
+++ src/main/java/com/nitramite/porssiohjain/services/WeatherControlService.java
@@ -40,6 +40,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
