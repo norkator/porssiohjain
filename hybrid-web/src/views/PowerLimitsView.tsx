bbb7445e2968a1e17279427f1972127a236d5670
norkator <martin.kankaanranta@hotmail.com>
2026-04-23 19:26:23 +0300

    add license texts

--- hybrid-web/src/views/PowerLimitsView.tsx
+++ hybrid-web/src/views/PowerLimitsView.tsx
@@ -1,3 +1,14 @@
+/*
+ * Pörssiohjain - Energy usage optimization platform
+ * Copyright (C) 2026  Martin Kankaanranta / Nitramite Tmi
+ *
+ * This source code is licensed under the Pörssiohjain Personal Use License v1.0.
+ * Private self-hosting for personal household use is permitted.
+ * Commercial use, resale, managed hosting, or offering the software as a
+ * service to third parties requires separate written permission.
+ * See LICENSE for details.
+ */
+
 import PageHeader from "@/components/PageHeader";
 import { createPowerLimit, fetchPowerLimits, formatDate, formatKw, type ApiPowerLimit } from "@/lib/automation-resources";
 import { FormEvent, useEffect, useState } from "react";
