4b40d3a9e37368b29739dd3363cfe86d07ef7d92
norkator <martin.kankaanranta@hotmail.com>
2026-08-12 19:51:58 +0300

    fixes and tweaks

---
+++ src/main/java/com/nitramite/porssiohjain/utils/JacksonConfig.java
@@ -0,0 +1,26 @@
+/*
+ * Pörssiohjain - Energy usage optimization platform
+ * Copyright (C) 2026  Martin Kankaanranta / Nitramite Tmi
+ *
+ * This source code is licensed under the Pörssiohjain Personal Use License v1.0.
+ * Private self-hosting for personal household use is permitted.
+ * Commercial use, resale, managed hosting, or offering the software as a
+ * service to third parties requires separate written permission.
+ * See LICENSE for details.
+ */
+
+package com.nitramite.porssiohjain.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper().findAndRegisterModules();
+    }
+
+}
